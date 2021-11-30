c021ed885b834c39060287822b2a16fa72e25750
Will <will@northflank.com>
2021-11-30 15:50:51 +0000

    Example deploying Plylst on Northflank via API

---
+++ README.md
@@ -0,0 +1,43 @@
+## Deploying Plylst on Northflank
+
+![Plylst on Northflank](header.png)
+
+[Plylst deployment tweet](https://twitter.com/Shpigford/status/1464262400362831876)
+
+"Won't be releasing it as a public service again unless I can figure out A) how to get server costs down from $300/mo to ~$50/mo"
+
+Below is a setup on Northflank costing ~ $55.
+
+To run the script you need a [Northflank API token](https://northflank.com/docs/v1/application/secure/manage-api-tokens) set as an environment variable `NF_TOKEN`.
+
+The index.js file makes a number of API requests to [Northflank](https://northflank.com) to create a project called [Plylst](https://github.com/Shpigford/plylst
+). It creates all the required infrastructure, builds, databases and secrets/connection strings.
+The steps below can also be followed in the UI. The estimated pricing is included on an item basis below.
+
+#### Plylst consists of:
+- 1 public git repository
+- 1 ruby web service running on port 8080
+- 1 ruby sidekiq background worker
+- 1 postgres database
+- 1 redis database
+
+The web and worker share the same codebase.
+
+The codebase can be built with a heroku/buildpacks:20 with ruby auto-detected.
+
+#### On Northflank I can configure:
+
+- A build service building from https://github.com/Shpigford/plylst ($1/pm)
+    - Initial builds take 4 minutes
+    - Subsequent builds take 1min30 with caching enabled
+
+- 2 deployment services
+    - Web: A deployment service auto deploying master branch from the builder with port 8080 publicly exposed ($16/pm)
+    - Worker: A deployment service auto deploying master branch from the builder with CMD over-ride: `bundle exec sidekiq -e ${RAILS_ENV:-production} -C config/sidekiq.yml` ($16/pm)
+
+- 2 Northflank addons:
+   - Redis: Managed Redis ($4/pm)
+   - Postgres: Managed Postgres ($10-16/pm) with 8GB $0.3GB from there
+
+- 1 manual job:
+   - Migrations: A manually triggered job to run migrations when required and on initialisation with CMD over-ride `rake --trace db:migrate`  ($1/pm)
---
+++ index.js
@@ -0,0 +1,252 @@
+import ApiClient, {
+    ApiClientInMemoryContextProvider
+} from '@northflank/js-client';
+
+(async () => {
+
+    const projectId = 'plylst'
+    const region = 'europe-west'
+    const apiToken = process.env.NF_TOKEN;
+
+    const resources = {
+        builder: 'nf-compute-200',
+        web: 'nf-compute-100-1',
+        migrations: 'nf-compute-100-1',
+        worker: 'nf-compute-100-1',
+        postgres: 'nf-compute-20',
+        redis: 'nf-compute-10',
+    }
+    const versions = {
+        branch: 'master',
+        postgres: '13.4.0',
+        redis: '6.2.5'
+    }
+
+    const resourceNames = {
+        builder: 'builder',
+        web: 'web',
+        migrations: 'migrations',
+        worker: 'worker',
+        postgres: 'postgres',
+        redis: 'redis',
+    }
+
+    const contextProvider = new ApiClientInMemoryContextProvider();
+    await contextProvider.addContext({
+        name: 'default',
+        token: apiToken,
+    });
+
+    const apiClient = new ApiClient(contextProvider);
+
+    await apiClient.create.project({
+        data: {
+            name: projectId,
+            region
+        },
+    });
+
+    await apiClient.create.service.build({
+        parameters: {
+            projectId,
+        },
+        data: {
+            name: "builder",
+            billing: {
+                deploymentPlan: resources.builder
+            },
+            vcsData: {
+                projectUrl: "https://github.com/Shpigford/plylst",
+                projectType: "github"
+            },
+            buildSettings: {
+                buildpack: {
+                    builder: "HEROKU_20",
+                    buildContext: "/"
+                }
+            }
+        }
+    });
+
+
+    await apiClient.start.service.build({
+        parameters: {
+            projectId,
+            serviceId: resourceNames.builder
+        },
+        data: {
+            branch: versions.branch
+        }
+    });
+
+
+    await apiClient.create.addon({
+        parameters: {
+            projectId
+        },
+        data: {
+            name: resourceNames.postgres,
+            type: "postgres",
+            version: versions.postgres,
+            billing: {
+                deploymentPlan: resources.postgres,
+                storage: 8192,
+                replicas: 1
+            }
+        }
+    });
+
+    await apiClient.create.addon({
+        parameters: {
+            projectId
+        },
+        data: {
+            name: resourceNames.redis,
+            type: "redis",
+            version: versions.redis,
+            billing: {
+                deploymentPlan: resources.redis,
+                storage: 4096,
+                replicas: 1
+            }
+        }
+    });
+
+
+    await apiClient.create.secret({
+        parameters: {
+            projectId
+        },
+        data: {
+            name: "prod-secrets",
+            description: "A description",
+            secretType: "environment",
+            priority: 10,
+            addonDependencies: [{
+                addonId: resourceNames.redis,
+                keys: [{
+                    keyName: "REDIS_MASTER_URL",
+                    aliases: [
+                        "REDIS_URL"
+                    ]
+                }]
+            },
+                {
+                    addonId: resourceNames.postgres,
+                    keys: [{
+                        keyName: "POSTGRES_URI",
+                        aliases: [
+                            "DATABASE_URL"
+                        ]
+                    }]
+                }
+            ],
+            data: {
+                spotify_key: process.env.spotify_key,
+                spotify_secret: process.env.spotify_secret
+            }
+        }
+    });
+
+
+    await apiClient.create.service.deployment({
+        parameters: {
+            projectId,
+        },
+        data: {
+            name: resourceNames.web,
+            billing: {
+                deploymentPlan: resources.web
+            },
+            deployment: {
+                instances: 1,
+                internal: {
+                    id: resourceNames.builder,
+                    branch: versions.branch,
+                    buildSHA: "latest"
+                }
+            },
+            ports: [{
+                name: "web",
+                internalPort: 8080,
+                public: true,
+                protocol: "HTTP"
+            }]
+        }
+    });
+
+
+    await apiClient.create.service.deployment({
+        parameters: {
+            projectId,
+        },
+        data: {
+            name: resourceNames.worker,
+            billing: {
+                deploymentPlan: resources.worker
+            },
+            deployment: {
+                instances: 1,
+                internal: {
+                    id: resourceNames.builder,
+                    branch: versions.branch,
+                    buildSHA: "latest"
+                }
+            }
+        }
+    });
+
+
+    await apiClient.create.job.manual({
+        parameters: {
+            projectId
+        },
+        data: {
+            name: resourceNames.migrations,
+            billing: {
+                deploymentPlan: resources.migrations
+            },
+            backoffLimit: 0,
+            activeDeadlineSeconds: 600,
+            deployment: {
+                instances: 1,
+                internal: {
+                    id: resourceNames.builder,
+                    branch: versions.branch,
+                    buildSHA: "latest"
+                }
+            }
+        }
+    });
+
+
+    await apiClient.update.job.cmdOverride({
+        parameters: {
+            projectId,
+            jobId: resourceNames.migrations
+        },
+        data: {
+            cmd: "rake --trace db:migrate"
+        }
+    });
+
+    await apiClient.update.service.cmdOverride({
+        parameters: {
+            projectId,
+            serviceId: resourceNames.worker
+        },
+        data: {
+            cmd: "bundle exec sidekiq -e ${RAILS_ENV:-production} -C config/sidekiq.yml"
+        }
+    });
+
+
+    await apiClient.start.job.run({
+        parameters: {
+            projectId,
+            jobId: resourceNames.migrations
+        }
+    });
+
+
+})();
